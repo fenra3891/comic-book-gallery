d907855b5c4b1cd026b8b7dd5a884c311bc89a72
Gary Coomber <Egil1983@hotmail.com>
2018-12-04 20:07:58 +0000

    Add comic book detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,12 +11,7 @@ namespace ComicBooksGallery.Controllers
         public ActionResult Detail()
         {
 
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
